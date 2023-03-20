b58b9f2229ca01f91af769664a460ecf93b7a33b
Barnabé SAMEL <108863369+BarnBS@users.noreply.github.com>
2023-03-20 15:46:01 +0100

    Update pizza_commandes_master_dev_2023.py

--- Algo training/Isograd_problèmes/pizza_commandes_master_dev_2023.py
+++ Algo training/Isograd_problèmes/pizza_commandes_master_dev_2023.py
@@ -1,5 +1,5 @@
 # Copier/coller le code ci-dessous à l'adresse suivante :
-# https://demo.isograd.com/runtest/QuestionDisplayer#output
+# https://www.isograd-testingservices.com/FR/solutions-challenges-de-code?cts_id=99&reg_typ_id=2&que_str_id=&cli_id=45alrk6jpdnaguf3oa3gto2875&rtn_pag=https%3A%2F%2Fwww.isograd-testingservices.com%2F%2FFR%2Fsolutions-challenges-de-code%3Fcts_id%3D33
 
 import sys
 
